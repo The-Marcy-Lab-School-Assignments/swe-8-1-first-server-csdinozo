c92e5d81229e3925a64f38f12c6251b96c4c1bd4
Catalina Dinozo <csdinozo@gmail.com>
2025-04-07 13:50:02 -0400

    routes

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -5,9 +5,9 @@ function App() {
                 <h1>My First API</h1>
                 <p>Welcome to my first API! This is a simple API that returns a random joke, can roll dice for you, and can provide you with a nice picture. Enjoy!</p>
                 <ul>
-                    <li>Visit <a href="/api/joke">/api/joke</a> for a funny joke</li>
-                    <li>Visit <a href="/api/picture">/api/picture</a> to see a nice picture</li>
-                    <li>Visit <a href="/api/rollDie">/api/rollDie</a> to roll a die. (Try <a href="/api/rollDie?quantity=3">/api/rollDie?quantity=3</a> to roll multiple dice!)</li>
+                    <li>Visit <a href="/joke">/joke</a> for a funny joke</li>
+                    <li>Visit <a href="/picture">/picture</a> to see a nice picture</li>
+                    <li>Visit <a href="/rollDie">/rollDie</a> to roll a die. (Try <a href="/rollDie?quantity=3">/rollDie?quantity=3</a> to roll multiple dice!)</li>
                 </ul>
             </main>
         </>
--- App/src/main.jsx
+++ App/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Joke from './components/Joke';
+import Picture from './components/Picture';
+import Dice from './components/Dice';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/joke" element={<Joke />} />
+            <Route path="/picture" element={<Picture />} />
+            <Route path="/dice-roller/:quantity" element={<Dice />} />
+        </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
